b02247169a2f756d72a81147ee8893aed01ef465
PinkMikalai <job.m.sashko@gmail.com>
2025-08-06 16:30:35 +0200

    verification des erreurs

--- assets/config/database.php
+++ assets/config/database.php
@@ -1,12 +1,12 @@
 <?php 
-
+// echo"hello rold";
 // logique de connexion a la database
 
 //information pour se connecter
 //l'endroit ou est ma database
 $host = "localhost";
 //le nom de la db
-$dbname = "users_db";
+$dbname = "table_users";
 //identifiant de connexion
 $username = "root";
 //mdp de connexion
@@ -19,22 +19,23 @@ $charset = "utf8mb4";
 //fonction qui crée et renvoi une connexion a la db
 function dbConnexion() {
     //transforme mes variable en global (accessible partout)
-    global $host, $dbname, $password, $username, $port, $charset;
+    global $host, $dbname, $password, $table_users, $port, $charset;
 
     try {
         //mes param de co
         $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset;port=$port";
         //fait mon object de co
-        $pdo = new PDO($dsn, $username, $password);
+        $pdo = new PDO($dsn, $table_users, $password);
         //comment recuperer les exception (erreurs)
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         //comment me renvoyer les données
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 // on return l information
+        var_dump($pdo);
         return $pdo; 
 
     } catch (PDOException $e) {
-        
+        die("et oui j ai fait de la merde". $e->getMessage());
     }
     
 }
